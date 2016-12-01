5f1d7eb4280aab0f6c908f0450c254b7d11e3e5d
Josh Walker <josh@kalamuna.com>
2016-12-01 13:10:52 -0800

    Adding routes and callbacks with HtmlResponse

---
+++ src/Controller/KalastaticServer.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * @file
+ * Contains \Drupal\kalastatic\Controller\KalastaticAdmin.
+ */
+
+namespace Drupal\kalastatic\Controller;
+
+use Drupal\Core\Controller\ControllerBase;
+use Drupal\Core\Render\HtmlResponse;
+use Symfony\Component\HttpFoundation\Request;
+
+class KalastaticServer extends ControllerBase {
+  public function content($type) {
+    kint($type);
+    // TODO: work out how we can load the html files.
+    return new HtmlResponse('<h1>It works, bitches</h1>');
+  }
+}
