4bd26119ae458773aa2c96f418bd358bafd8d535
Josh Walker <josh@kalamuna.com>
2016-12-02 18:05:26 -0800

    Adding todo so I remember what to do.

--- src/Controller/KalastaticServer.php
+++ src/Controller/KalastaticServer.php
@@ -13,7 +13,8 @@ use Symfony\Component\HttpFoundation\Request;
 class KalastaticServer extends ControllerBase {
   public function content($type) {
     kint($type);
-    // TODO: work out how we can load the html files.
+    // TODO: work out how we can load the html files. We need to acheive what 
+    // kalastatic_serve_ks_files() is doing from the D7 version.
     return new HtmlResponse('<h1>It works, bitches</h1>');
   }
 }
