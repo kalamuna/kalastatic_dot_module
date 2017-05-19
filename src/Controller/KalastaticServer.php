927750353a49f4b3d4220bf354cdbafa7a3f6695
Josh <josh@kalamuna.com>
2017-05-19 15:04:45 -0700

    Handling response for css, js and images

--- src/Controller/KalastaticServer.php
+++ src/Controller/KalastaticServer.php
@@ -13,66 +13,12 @@ use Drupal\Core\Url;
 use Drupal\Core\Asset;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\BinaryFileResponse;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
 
 class KalastaticServer extends ControllerBase {
   public function content() {
-
-
-    // return array(
-    //   '#markup' => 'this is working',
-    // );
-
-    //$library_discovery = \Drupal::service('library.discovery');
-    //$libraries = $this->libraryDiscoveryParser->buildByExtension('css_js_settings');
-
-    // Get the path and split it up into an array. Note that due to our (not so)
-    // fancy path processor in KalastaticPathProcessor everything underneath
-    // /kalastatic/ is coming in split up with colons. This brings back the
-    // 'catch all' functionality of hook_menu that we relied on in D7 to hanlde
-    // everything below a given path.
-    // $path = \Drupal::request()->getpathInfo();
-    // $args = explode('/', $path);
-    // $args = explode(':', $args[2]);
-    // $path = implode('/', $args);
-
-    //xdebug_break();
-    //$path = 'index.html';
-    //$needs_headers = FALSE;
-    // Check if we're visiting something deeper than the styleguide or prototype
-    // home page.
-    //if (isset($args[3]) && !empty($args[3])) {
-      //$suffix = '';
-
-      // Regex on the last arg to see if we're trying to load a page or a file.
-      //$last_arg = end($args);
-      //preg_match('/\.[^\.]+$/i', $last_arg, $ext);
-
-      // if (isset($ext[0])) {
-      //   // This is a file so we need to add headers for it's mime type before we
-      //   // return the file.
-      //   $needs_headers = TRUE;
-      // }
-      // else {
-      //   // Assume we're loading the html page.
-      //   $suffix = '/' . $path;
-      //}
-
-      // if (isset($ext[0]) && $ext[0] == '.html') {
-      //   $suffix = '/' . $path;
-      // }
-
-      // $build_args = $args;
-      // unset($build_args[0]);
-      // //unset($build_args[1]);
-      // $path = implode('/', $build_args);
-    // }
-    // else {
-    //   // There isn't a requested page, so redirect the user to index.html
-    //   // homepage.
-    //   $path = Url::fromUri('base:/kalastatic/prototype/index.html');
-    //   return new RedirectResponse($path->toString());
-    // }
     xdebug_break();
     $build_path = $this->getBuildPath();
 
@@ -102,19 +48,20 @@ class KalastaticServer extends ControllerBase {
       $file = $build_path . '/' . $path . '/index.html';
     }
 
-    //xdebug_break();
-
-    // Add headers if needed.
-    //if ($needs_headers) {
-      //$mime_type = file_get_mimetype($file);
-      //drupal_add_http_header('Content-Type', $mime_type);
-      //TODO: headers and mime types need to be handed in need to be handled
-      // in an event subscriber http://drupal.stackexchange.com/questions/198850/replacement-for-drupal-add-http-header
-    //}
-
     if (file_exists($file)) {
       $file_contents = file_get_contents($file);
-      // We have a file to serve so let's do it!
+
+      // We have a file, now lets see if we need to set the headers
+      if ('html' == $ext[0]) {
+        // This is html file so return a HtmlResponse.
+        $output = new HtmlResponse($file_contents);
+      }
+      else {
+        // This is another type of file so I guess handle it as binary? Seems to
+        // work for now.
+        return new BinaryFileResponse($file);
+      }
+
       $output = new HtmlResponse($file_contents);
     }
     elseif (file_exists($build_path)){
